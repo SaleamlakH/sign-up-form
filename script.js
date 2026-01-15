aadff057beb6cdb32586ef0a1690dbe8dd6d0e53
SaleamlakH <saleamlak.7h@gmail.com>
2026-01-15 16:55:42 +0300

    Refactor: Replace label elements with spans for input hints
    Using labels for hints is not sematic and affect the accessibility.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const password = document.querySelector('#password');
 const confirmPassword = document.querySelector('#confirm-password');
-const errorLabel = document.querySelector('#confirm-password + label');
+const passwordMatchHint = document.querySelector('.password-match-hint');
 const submitBtn = document.querySelector('#submit');
 
 confirmPassword.addEventListener('focusout', validatePasswordConfirmation);
@@ -9,8 +9,8 @@ submitBtn.addEventListener('click', validatePasswordConfirmation);
 function validatePasswordConfirmation(event) { 
     if (confirmPassword.value !== password.value) {
         if (event.currentTarget.type === 'submit') event.preventDefault();
-        errorLabel.style.color = 'hsl(0 100 60)';
+        passwordMatchHint.style.color = 'hsl(0 100 60)';
     } else {
-        errorLabel.style.color = 'hsl(0 0 60)';
+        passwordMatchHint.style.color = 'hsl(0 0 60)';
     }
 }
